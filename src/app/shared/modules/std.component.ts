aa9db7d159a142ec04cc50838a085966188674ad
ganeshbhalke <ganeshbhalke2004@gmail.com>
2026-04-17 07:52:46 +0530

    publish

--- src/app/shared/modules/std.component.ts
+++ src/app/shared/modules/std.component.ts
@@ -1,4 +1,25 @@
 export interface Itodo{
     todoItem : string
     todoId : string
+};
+
+export interface Ifruits{
+    todoItem : string
+    todoId : string
 }
+
+export interface Icricket{
+    todoItem : string
+    todoId : string
+}
+
+export interface IFlowers{
+    todoItem : string
+    todoId : string
+}
+
+export interface IMobile{
+    todoItem : string
+    todoId : string
+}
+
